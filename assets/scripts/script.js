8f157a1c07243d45c4e4bd7dc0c80de322123c9f
Anindita Som <aninditasom2020@gmail.com>
2020-10-15 06:58:22 +0000

    added latitude and longitude for all nordic countries

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -39,20 +39,34 @@ function showSlides(n) {
 // Google Maps Coding
 
 const ApiKey = "AIzaSyCeetcMkyfLKX-96r4V__MikIyrXS17Hjw";
+const norwayLat = 60.765172;
+const norwayLng = 8.095888;
+const swedenLat = 57.522434;
+const swedenLng = 14.775575;
+const denmarkLat = 56.291470;
+const denmarkLng = 10.591553;
+const greenlandLat = 75.278541;
+const greenlandLng = -43.408019;
+const finlandLat = 63.000747;
+const finlandLng = 26.992372;
+const faroeLat = 61.8948697;
+const faroeLng = -8.0752449;
+const alandLat = 60.2588775;
+const alandLng = 19.227217;
 
 let map;
 let service;
 let infowindow;
 
 function initMap() {
-  const sydney = new google.maps.LatLng(-33.867, 151.195);
+  const Denmark = new google.maps.LatLng(denmarkLat, denmarkLng);
   infowindow = new google.maps.InfoWindow();
   map = new google.maps.Map(document.getElementById("map"), {
-    center: sydney,
+    center: Denmark,
     zoom: 15,
   });
   const request = {
-    query: "Museum of Contemporary Art Australia",
+    query: "Art Galleries of Denmark",
     fields: ["name", "geometry"],
   };
   service = new google.maps.places.PlacesService(map);
