43c6870d9e00b71bd24515dbbfa80051fd48638e
Anindita Som <aninditasom2020@gmail.com>
2020-10-22 23:13:11 +0000

    Add markers to the maps

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -47,166 +47,119 @@ const swedenLat = 57.522434;
 const swedenLng = 14.775575;
 const denmarkLat = 56.291470;
 const denmarkLng = 10.591553;
-const greenlandLat = 75.278541;
-const greenlandLng = -43.408019;
+const greenlandLat = 66.905645;
+const greenlandLng = -34.924484;
 const finlandLat = 63.000747;
 const finlandLng = 26.992372;
-const faroeLat = 61.8948697;
-const faroeLng = -8.0752449;
-const alandLat = 60.2588775;
-const alandLng = 19.227217;
+const faroeLat = 61.848012;
+const faroeLng = -6.865365;
+const alandLat = 60.245031;
+const alandLng = 19.953666;
 
 //Global Variable for countrySearch
 var searchCountry = sessionStorage.getItem("searchCountry");
-
 var map;
-let service;
-let infowindow;
-let country;
+var infowindow;
 
 if (searchCountry === null || searchCountry === "null"){
     document.getElementById("selected_country").innerText = " : -- No Selection Made --";
 }
 else{
     document.getElementById("selected_country").innerText = " : " + searchCountry;
-    setCountry(searchCountry);
+    setCountry(searchCountry); // highlights the country thumbnail
 }
+
 function initMap() {
-   
+    infowindow = new google.maps.InfoWindow();
     switch(searchCountry){
         case 'Aland':
-            map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15,
-                center: {
-                    lat: alandLat,
-                    lng: alandLng
-                }
-            });
-            // country = new google.maps.LatLng(alandLat, alandLng);
+            var country = new google.maps.LatLng(alandLat, alandLng);
             break;
         case 'Denmark': 
-            map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15,
-                center: {
-                    lat: denmarkLat,
-                    lng: denmarkLng
-                }
-            });
-   
-            //country = new google.maps.LatLng(denmarkLat, denmarkLng);
+            var country = new google.maps.LatLng(denmarkLat, denmarkLng);
             break;
         case 'Norway':
-            map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15,
-                center: {
-                    lat: norwayLat,
-                    lng: norwayLng
-                }
-            });
-            //console.log("Norway...." + norwayLat + "..." + norwayLng);
-            //country = new google.maps.LatLng(norwayLat, norwayLng);
+            var country = new google.maps.LatLng(norwayLat, norwayLng);
             break;
         case 'Sweden':
-            map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15,
-                center: {
-                    lat: swedenLat,
-                    lng: swedenLng
-                }
-            });
-            //country = new google.maps.LatLng(swedenLat, swedenLng);
+            var country = new google.maps.LatLng(swedenLat, swedenLng);
             break;
         case 'Greenland':
-            map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15,
-                center: {
-                    lat: greenlandLat,
-                    lng: greenlandLng
-                }
-            });
-            //country = new google.maps.LatLng(greenlandLat, greenlandLng);
+            var country = new google.maps.LatLng(greenlandLat, greenlandLng);
             break;
         case 'Finland':
-            map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15,
-                center: {
-                    lat: finlandLat,
-                    lng: finlandLng
-                }
-            });
-            //country = new google.maps.LatLng(finlandLat, finlandLng);
+            var country = new google.maps.LatLng(finlandLat, finlandLng);
             break;
         case 'Faroe':
-            map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15,
-                center: {
-                    lat: faroeLat,
-                    lng: faroeLng
-                }
-            });    
-        
-        //country = new google.maps.LatLng(faroeLat, faroeLng);
+            var country = new google.maps.LatLng(faroeLat, faroeLng);
             break;
-    
+        default:
+            var country = new google.maps.LatLng(59.559208, 11.244315);
     }
-    // console.log(country);
-   // infowindow = new google.maps.InfoWindow();
-   // map = new google.maps.Map(document.getElementById("map"), {
-   //     center: country,
-   //     zoom: 10,
-   // });
+   
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: country
+    });    
 
+    //console.log(map);
     //business_status, formatted_address, geometry, icon,name, 
     // permanently_closed (deprecated), photos, place_id, plus_code, types
    
-
-    const request = {
-        query: "Art Galleries and Museums of " + searchCountry,
-        fields: ["place_id", "name", "geometry", "formatted_address", "business_status", "icon"],
+    var request = {
+        location: country,
+        radius: '1000',
+    
+        query: searchCountry + " Art galleries + museum",
+        fields: ["place_id", "name", "geometry", "formatted_address", "business_status", "photos"],
     };
-    service = new google.maps.places.PlacesService(map);
-    service.findPlaceFromQuery(request, (results, status) => {
+    
+    var service = new google.maps.places.PlacesService(map);
+     
+    service.nearbySearch(request, (results, status) => {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-            console.log(results.length);
+            console.log("nearby search" + results.length);
             
             for (let i = 0; i < results.length; i++) {
-                console.log(results[i]);
-                fetchSearchResults(results[i]);
-                createMarker(results[i].geometry.location);
+               // console.log(results[i]);
+                //fetchSearchResults(results[i]);
+                createMarker(results[i],i);
             }
-        map.setCenter(results[0].geometry.location);
+            //map.setCenter(results[0].geometry.location);
         }
     });
 }
 
-function createMarker(place) {
+function createMarker(place,i) {
 
     var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-    console.log(place); 
-    // var markers = locations.map(function(place, i) {
-    //     return new google.maps.Marker({
-    //         position: location,
-    //         label: labels[i % labels.length]
-    //     });
-    // });
-
-    // var markerCluster = new MarkerClusterer(map, markers, {
-    //     imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-    // });
+   // console.log(place); 
+    const marker = new google.maps.Marker({map,
+        position: place.geometry.location,
+        label: labels[i % labels.length]
+    });
     
-    // var marker = new google.maps.Marker({map,
-    // position: place.geometry.location,
-    // label: labels[i % labels.length]
-//   });
-  //google.maps.event.addListener(marker, "click", () => {
-  //  infowindow.setContent(place.name);
-  //  infowindow.open(map);
-  //});
+    google.maps.event.addListener(marker, "click", () => {
+        infowindow.setContent(place.name);
+        infowindow.open(map);
+  });
 }
+
+    /*function callback(results, status) {
+         if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                fetchSearchResults(results[i]);
+                // createMarker(results[i]);
+            }
+        }
+    }*/
+    
+
 
 // **************** End Google Map *****************
  
  function setCountry(countryName){
+     // This function highlights the country thumbnail
     
     let country = document.getElementById(countryName);
     
@@ -230,6 +183,9 @@ function clearSession()
     sessionStorage.removeItem("searchCountry");
     sessionStorage.clear;
 }
+function addtoWishList(){
+    alert("this is under construction now ");
+}
 
 function fetchSearchResults(place){
     
@@ -237,18 +193,15 @@ function fetchSearchResults(place){
        // $.getJSON(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY`);}
     // .then{}
     
-    $("#GallerySearchResults").html(`
+    $("#GallerySearchResults").html(`<div class = "row">
         <span class="col-md-8">
             Name : ${place.name} &nbsp; | &nbsp; Address:${place.formatted_address} <br> Business Status: ${place.business_status}
-           <br> <img src="${place.icon}" alt="thumbnail image of gallery">
+           <br> <img src="" alt="thumbnail image of gallery">
         </span>
         <span class="col-md-4">
-            <button type="button" name="myWish" id="myWish" onclick="addtoWishList(${place.place_id})" >add to wishlist</button>
+            <button type="button" name="myWish" id="myWish" onclick="addtoWishList()" ><i class="fas fa-heart wishlist"></i></button>
            
-        </span>
+        </span></div>
     `);
 }
 
-function addtoWishList(placeId){
-    alert("this is under construction now for Place-ID - " + placeId);
-}
