8b5206262fd9880873ac0e1bf44e0521baa037a1
Anindita Som <aninditasom2020@gmail.com>
2020-10-28 10:56:35 +0000

    remove wishlist items in wishlist page

--- assets/scripts/wishlist_scripts.js
+++ assets/scripts/wishlist_scripts.js
@@ -30,37 +30,59 @@ function fetchfromWishList(){
    // alert("fetch from Wishlist");
     //console.log(localStorage.getItem("WishList"));
     var myWishlist = JSON.parse(localStorage.getItem("WishList"));   
-    alert(myWishlist.length);
+    console.log("elements in wishlist #" + myWishlist.length);
+    //alert(myWishlist.length);
     for (let i=0; i < myWishlist.length ; i++){
         var newRow = document.createElement("div");
         newRow.className = "row-position";
         newRow.classList.add = "row";
         newRow.classList.add = "no-gutters";
-        
-        newRow.innerHTML = parseWishlist(myWishlist[i]);
+        var divId = "div" + i;
+        newRow.setAttribute("id", divId);
+        newRow.innerHTML = parseWishlist(myWishlist[i],i);
         console.log(newRow);
         document.getElementById("WishlistDisplay").appendChild(newRow);
     }
 }
 
-function parseWishlist(wish){
+function parseWishlist(wish, i){
     //console.log(wish);
     var htmlOfPlaces = "";
             var country = wish[0];
             var objPlaces = wish[1][0];
+            
             for (let place in objPlaces)
             {
+                
                 //console.log(objPlaces[place]);
                 htmlOfPlaces += ( objPlaces[place] + "&nbsp; | &nbsp;");
+        
             }
-                 htmlOfPlaces += "<span class='icon-position'><a onclick='deleteFromWishList()'><i class='far fa-trash-alt'></i></a></span>"
+                 htmlOfPlaces += "<span class='icon-position'><a onclick='deleteFromWishList(" + i + ")'><i class='far fa-trash-alt'></i></a></span>"
     return  "<b>" + country + "</b>&nbsp; - &nbsp;" + htmlOfPlaces ;
     //console.log(country);
     //console.log(htmlOfPlaces);
 }
 
-function deleteFromWishList()
+function deleteFromWishList(counter)
 {
     var response = confirm("This record will be deleted, do you wish to continue? ");
-    console.log(response);
+    var myWishlist = JSON.parse(localStorage.getItem("WishList"));   
+    if (response){
+        // console.log(counter);
+        // console.log(myWishlist);
+        myWishlist.splice(counter,1);
+        //console.log(myWishlist);
+        var divId = "div" + counter;
+        console.log(divId);
+        localStorage.setItem ("WishList", JSON.stringify(myWishlist));
+        
+        var element = document.getElementById(divId);
+        element.parentNode.removeChild(element);
+
+        console.log("removed ##" + element);
+    }else{
+        alert("Not removed!");
+    }
+
 }
