9f16ebe30baa4bc876539dff46b60a7c8b7a1f73
Anindita Som <aninditasom2020@gmail.com>
2020-10-30 00:21:00 +0000

    email wishlist in a table format using emailjs

--- assets/scripts/wishlist_scripts.js
+++ assets/scripts/wishlist_scripts.js
@@ -1,36 +1,10 @@
 
-/*const locations = [finland, [{
-                                name: "some name", 
-                                coords: {lng: 2.34, lat: 234.3434}, 
-                                description: "description of item", 
-                                address: "Address Line 1, Line 2"
-                            },
-                            {
-                                name: "other name", 
-                                coords: {lng: 2.34, lat: 234.3434}, 
-                                description: "description of item", 
-                                address: "Address Line 1, Line 2"
-                            }],
-                [denmark,   [{
-                                name: "other name", 
-                                coords: {lng: 2.34, lat: 234.3434}, 
-                                description: "description of item", 
-                                address: "Address Line 1, Line 2"
-                            },
-                            {
-                                name: "other name", 
-                                coords: {lng: 2.34, lat: 234.3434}, 
-                                description: "description of item", 
-                                address: "Address Line 1, Line 2"
-                            }]
-                        ]];
-*/
 
 function fetchfromWishList(){
    // alert("fetch from Wishlist");
     //console.log(localStorage.getItem("WishList"));
     var myWishlist = JSON.parse(localStorage.getItem("WishList"));   
-    console.log("elements in wishlist #" + myWishlist.length);
+   // console.log("elements in wishlist #" + myWishlist.length);
     //alert(myWishlist.length);
     for (let i=0; i < myWishlist.length ; i++){
         var newRow = document.createElement("div");
@@ -40,7 +14,7 @@ function fetchfromWishList(){
         var divId = "div" + i;
         newRow.setAttribute("id", divId);
         newRow.innerHTML = parseWishlist(myWishlist[i],i);
-        console.log(newRow);
+        //console.log(newRow);
         document.getElementById("WishlistDisplay").appendChild(newRow);
     }
 }
@@ -74,15 +48,61 @@ function deleteFromWishList(counter)
         myWishlist.splice(counter,1);
         //console.log(myWishlist);
         var divId = "div" + counter;
-        console.log(divId);
+        //console.log(divId);
         localStorage.setItem ("WishList", JSON.stringify(myWishlist));
         
         var element = document.getElementById(divId);
         element.parentNode.removeChild(element);
 
-        console.log("removed ##" + element);
+ //       console.log("removed ##" + element);
     }else{
         alert("Not removed!");
     }
 
+}
+
+function sendMail(){
+   var myList = JSON.parse(localStorage.getItem("WishList"));
+   //console.log(document.getElementById("email").value);
+   (function(){
+        emailjs.init("user_9tv1BSdrIPXy39aw6nHuB");
+        
+        var templateParams = {
+            from_name: "Nordic Art Gallery Search",
+            from_email: "Arts & More",
+            to_name: "Guest",
+            Galleries_wishlist: parseMyList(myList),
+            to_email: document.getElementById("email").value
+        }
+        emailjs.send('gallerySearch', 'wishlist', templateParams)
+            .then(function(response) {
+            alert('SUCCESS!'+ ": " + response.status + ", " + response.text);
+            }, function(error) {
+            alert('FAILED...' + error);
+            });
+   })();
+}
+
+function parseMyList(list){
+//    console.log(list + " ####" + list.length);
+    
+
+    var htmlString = "<br><table id='emailTable'><tr><th>Country</th><th>Place</th><th>Address</th><th>Status</th></tr>" //</th></thead></table>"
+    
+    
+    for (let i = 0; i < list.length ; i++){
+        htmlString += "<tr><td style='padding: 10px'>" + list[i][0] + "</td>";
+    
+        var objPlaces = list[i][1][0];
+        //console.log(objPlaces);
+        for (let place in objPlaces){
+           // ( objPlaces[place] + "&nbsp; | &nbsp;");
+            htmlString += ( "<td style='padding: 6px'>" + objPlaces[place] + "</td>" ); 
+        }
+         htmlString += "</tr>";
+    }
+    htmlString += "</table><br>";
+   // console.log (htmlString); 
+
+    return htmlString;
 }
